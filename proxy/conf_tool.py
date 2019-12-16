d90b150678530d3d2e3ed83353d80b9083be257c
smaug.jiao <smaug.jiao@topnetwork.org>
2019-12-16 16:24:04 +0800

    rename conf_tool.py; add argparse

--- proxy/conf_tool.py
+++ proxy/conf_tool.py
@@ -17,20 +17,21 @@ import copy
 import json
 import threading
 import operator
+import argparse
 
 gconfig = {
         'global_sample_rate': 100,  # sample_rate%
         'alarm_pack_num': 1,   # upload alarm size one time
         'grep_broadcast': {
             'start': 'true',
-            'sample_rate': 100,
+            'sample_rate': 50,
             'alarm_type': 'packet',
             'network_focus_on': ['660000', '680000', '690000'], # src or dest
             'network_ignore':   ['670000'],  # src or dest
             },
         'grep_point2point': {
             'start': 'false',
-            'sample_rate': 100,
+            'sample_rate': 50,
             'alarm_type': 'packet',
             'network_focus_on': ['660000', '680000', '690000'], # src or dest
             'network_ignore':   ['670000'],  # src or dest
@@ -74,7 +75,34 @@ def update_config():
         print('update remote config exception: {0}'.format(e))
         return False
 
+def get_config():
+    url = 'http://127.0.0.1:9090/api/config/'
+    my_headers = {
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+            'Content-Type': 'application/json;charset=UTF-8',
+            }
+    config = {}
+    try:
+        res = requests.get(url, headers = my_headers, timeout = 5)
+        if res.status_code == 200:
+            if res.json().get('status') == 0:
+                slog.info("get remote config ok, response: {0}".format(res.text))
+                print("get remote config ok, response: {0}".format(json.dumps(res.json(), indent = 4)))
+                return True
+    except Exception as e:
+        print("exception: {0}".format(e))
+        return False
 
     
 if __name__ == "__main__":
-    update_config()
+    parser = argparse.ArgumentParser()
+    parser.description='TOP-Argus config tool，更新配置文件到配置中心，查看配置中心的配置文件'
+    parser.add_argument('-u', '--update', help='update config server', default = False)
+    parser.add_argument('-g', '--get',    help='get config from server', default = False)
+    args = parser.parse_args()
+
+    if args.update == True:
+        update_config()
+
+    if args.get == True:
+        get_config()
